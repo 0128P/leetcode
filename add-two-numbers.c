9cb2096b7c1ccc72cc80c23a73352d045c7a008e
0128P <pnu0128@gmail.com>
2025-09-21 13:25:00 -0400

    solveLater

---
+++ add-two-numbers.c
@@ -0,0 +1,12 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+	*head1 = l1;
+	*head2 = l2;
+	
+}
