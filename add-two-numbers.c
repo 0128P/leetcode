3e8c1ff444f7adfd005a461c60f81ca5b682f1ee
0128P <pnu0128@gmail.com>
2025-09-22 05:07:41 -0400

    250922

--- add-two-numbers.c
+++ add-two-numbers.c
@@ -6,7 +6,26 @@
  * };
  */
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
-	*head1 = l1;
-	*head2 = l2;
-	
+	struct ListNode head;
+	struct ListNode *current = &head;
+	int carry = 0;
+
+	while(l1 != NULL || l2 != NULL || carry) {
+		int sum = carry;
+		if(l1 != NULL) {
+			sum += l1->val;
+			l1 = l1->next;
+		}
+		if(l2 != NULL) {
+			sum += l2->val;
+			l2 = l2->next;
+		}
+		carry = sum / 10;
+		struct ListNode *node = (struct ListNode*)malloc(sizeof(struct ListNode));
+		node->val = sum % 10;
+		node->next = NULL;
+		current->next = node;
+		current = node;
+	}
+	return head.next;
 }
